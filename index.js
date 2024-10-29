a3ff754da05e91fc56e3dbd22ad6c868159ba033
fipso <fipso@users.noreply.github.com>
2024-10-29 16:28:44 +0100

    feat: parse code from json body; remove preconnect

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-//import http from 'http';
-const http = require("http");
+const RUNNER_HTTP_MODULE = require("http");
 
 const BLAZED_RUNNER_PORT = process.env.BLAZED_RUNNER_PORT
   ? parseInt(process.env.BLAZED_RUNNER_PORT)
   : 42069;
 
-// console.log wrapper with timestamp
-// const oldConsoleLog = console.log;
-// console.log = function(...args) {
-//   oldConsoleLog(`[${new Date().toLocaleString()}]`, ...args);
-// };
-
-const BLAZED_RUNNER_SERVER = http.createServer(function(REQUEST, RESPONSE) {
-  if (REQUEST.method !== "POST") {
-    RESPONSE.write("405");
-    RESPONSE.end();
-    return;
-  }
-
-  if (REQUEST.url !== "/") {
-    RESPONSE.write("404");
-    RESPONSE.end();
-    return;
-  }
-
-  let BODY = "";
-  REQUEST.on("readable", function() {
-    // Skip null
-    const DATA = REQUEST.read();
-    if (DATA === null) {
+const BLAZED_RUNNER_SERVER = RUNNER_HTTP_MODULE.createServer(
+  function(REQUEST, RESPONSE) {
+    if (REQUEST.method !== "POST") {
+      RESPONSE.write("405");
+      RESPONSE.end();
       return;
     }
-    BODY += DATA;
-  });
-  REQUEST.on("end", function() {
-    RESPONSE.write('{"status": "success"}');
-    RESPONSE.end();
 
-    // Close the server
-    BLAZED_RUNNER_SERVER.close();
+    if (REQUEST.url !== "/") {
+      RESPONSE.write("404");
+      RESPONSE.end();
+      return;
+    }
 
-    // Run with source-map url
-    console.log("[BLAZED.sh] Executing your script...")
-    eval(BODY + "//# sourceURL=script.js");
-  });
-});
+    let BODY = "";
+    REQUEST.on("readable", function() {
+      // Skip null
+      const DATA = REQUEST.read();
+      if (DATA === null) {
+        return;
+      }
+      BODY += DATA;
+    });
+    REQUEST.on("end", function() {
+      const JSON_BODY = JSON.parse(BODY);
+      const SCRIPT = JSON_BODY.code;
+      RESPONSE.write('{"status": "success"}');
+      RESPONSE.end();
+
+      // Close the server
+      BLAZED_RUNNER_SERVER.close();
+
+      // Run with source-map url
+      console.log("[BLAZED.sh] Executing your script...");
+      eval(SCRIPT + "//# sourceURL=script.js");
+    });
+  },
+);
 
 BLAZED_RUNNER_SERVER.listen(BLAZED_RUNNER_PORT);
